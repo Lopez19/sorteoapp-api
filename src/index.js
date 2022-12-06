a1516254b728612584b9a327c17fd58b6f641b02
Horacio López <74926954+Lopez19@users.noreply.github.com>
2022-12-06 20:40:14 +0000

    setup port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import app from "./app";
 import connectDB from "./database";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Port
+const port = process.env.PORT || 3000;
 
 // Connect to database
 connectDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port", 3000);
+app.listen(port, () => {
+  console.log("Server is running on port", port);
 });
